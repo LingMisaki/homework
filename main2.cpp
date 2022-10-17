fb2a572253d6c14d7fc068a9c97101877546753f
LingMisaki <lemon__wind@outlook.com>
2022-10-17 10:45:35 -0700

    almost finish hw2 pro2

--- main2.cpp
+++ main2.cpp
@@ -1,57 +1,36 @@
-#include <opencv2/opencv.hpp>
+#include<eigen3/Eigen/Core>
+#include<eigen3/Eigen/Geometry>
+#include<iostream>
+#include<opencv2/opencv.hpp>
+#include<cstdio>
+int cnt;
+double x,y,z;
 int main(){
-    cv::VideoCapture capture("../IMG_8511.mp4");
-    cv::Mat src;
-    int cnt=0;
-    bool flag=false;//标识前一帧有无刚射出的子弹
-    while(capture.read(src)){
-        
-        cv::Mat hsv;
-        cv::cvtColor(src, hsv, cv::COLOR_BGR2HSV);
-        cv::Mat hsv_;
-        cv::inRange(hsv, cv::Scalar(70, 0, 0), cv::Scalar(90, 255, 255),hsv_);
-        cv::Mat blur_result;
-        cv::medianBlur(hsv_, blur_result, 3);
-        
-
-        cv::Mat morph_result;
-        cv::Mat element=getStructuringElement(cv::MORPH_RECT,cv::Size(5,5));
-        cv::morphologyEx(blur_result,morph_result,cv::MORPH_CLOSE,element);
-   
-        std::vector<std::vector<cv::Point>> contours;
-        std::vector<cv::Vec4i> hierachy;
-        cv::findContours(morph_result,contours,hierachy,cv::RETR_TREE,cv::CHAIN_APPROX_NONE);
-
-        bool isempty=true;//标记当前帧是否有刚射出的子弹
-        int fontFace = cv::FONT_HERSHEY_SCRIPT_SIMPLEX;
-        cv::Point2f dot(80,80);
-        for(int i=0;i<contours.size();i++){
-            cv::putText(src,std::to_string(cnt),dot,fontFace,2,cv::Scalar(255,255,255),3);
-            cv::RotatedRect rr =cv::minAreaRect(contours[i]);
-            //if(cv::contourArea(contours[i])<10)
-            //    continue;
-            if(std::max(rr.size.height,rr.size.width)/std::min(rr.size.height,rr.size.width)<7)
-                continue;
-            if(rr.angle<-10&&rr.angle>-15&&rr.size.height>100){
-                if(flag==false)
-                    cnt++;//前一帧没有子弹，此帧有子弹，则计数加一
-                isempty=false;
-                cv::putText(src,std::to_string(cnt),rr.center,fontFace,2,cv::Scalar(255,255,255),3);
-            }                
-            cv::Point2f pts[10];
-            rr.points(pts);
-            cv::line(src,pts[0],pts[1],cv::Scalar(0,0,255),2);
-            cv::line(src,pts[1],pts[2],cv::Scalar(0,0,255),2);
-            cv::line(src,pts[2],pts[3],cv::Scalar(0,0,255),2);
-            cv::line(src,pts[3],pts[0],cv::Scalar(0,0,255),2);
-            cv::circle(src,rr.center,8,cv::Scalar(255,255,255),2);
-            //cv::putText(src,std::to_string(rr.angle),rr.center,fontFace,1,cv::Scalar(255,255,255));
-        }
-        if(isempty)
-            flag=false;
-        cv::imshow("src",src);
-        cv::waitKey(50);
+    std::freopen("../points.txt","r",stdin);
+    Eigen::Matrix4d out_para;
+    Eigen::Matrix<double,3,4> in_para;
+    in_para<<400., 0., 190., 0.,    
+             0., 400., 160.,  0., 
+             0., 0.,   1.,   0.;
+    //Eigen::Quaterniond in_para_qua(-0.5,0.5,0.5,-0.5);//0.5i + 0.5j + -0.5k + -0.5
+    //std::cout<<in_para_qua.matrix();
+    out_para<<0.,  0., -1., 2.,
+              1.,  0.,  0., 2.,
+              0., -1.,  0., 2.,
+              0., 0.,   0., 1.;
+    std::cin>>cnt;
+    Eigen::Vector4d inputt;
+    Eigen::Vector3d outputt;
+    cv::Mat result(320,380,CV_8UC3);
+    for(int i=1;i<=cnt;i++){
+        std::cin>>x>>y>>z;
+        inputt<<x,y,z,1;
+        outputt=in_para*out_para*inputt/z;
+        cv::Point2d centre(outputt(1,1),outputt(2,1));
+        std::cout<<outputt<<std::endl;
+        cv::circle(result,centre,1,cv::Scalar(0,255,0));
     }
-
-    return 0;
+    std::fclose(stdin);
+    cv::imshow("result",result);
+    cv::waitKey(0);
 }
