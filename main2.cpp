b3bea46050ff5b2135f462a266ad36e1415e4ae9
LingMisaki <lemon__wind@outlook.com>
2022-10-17 22:19:59 -0700

    finish hw2 pro2

--- main2.cpp
+++ main2.cpp
@@ -12,23 +12,23 @@ int main(){
     in_para<<400., 0., 190., 0.,    
              0., 400., 160.,  0., 
              0., 0.,   1.,   0.;
-    //Eigen::Quaterniond in_para_qua(-0.5,0.5,0.5,-0.5);//0.5i + 0.5j + -0.5k + -0.5
-    //std::cout<<in_para_qua.matrix();
     out_para<<0.,  0., -1., 2.,
               1.,  0.,  0., 2.,
               0., -1.,  0., 2.,
-              0., 0.,   0., 1.;
+              0., 0.,   0., 1.; 
     std::cin>>cnt;
     Eigen::Vector4d inputt;
     Eigen::Vector3d outputt;
     cv::Mat result(320,380,CV_8UC3);
     for(int i=1;i<=cnt;i++){
         std::cin>>x>>y>>z;
-        inputt<<x,y,z,1;
-        outputt=in_para*out_para*inputt/z;
-        cv::Point2d centre(outputt(1,1),outputt(2,1));
-        std::cout<<outputt<<std::endl;
-        cv::circle(result,centre,1,cv::Scalar(0,255,0));
+        inputt<<x,y,z,1.;
+        Eigen::Vector4d Zc_tmp=out_para*inputt;
+        double Zc=Zc_tmp(2,0);
+        outputt=in_para*out_para*inputt/Zc;
+        std::cout<<outputt(0,0)<<" "<<outputt(1,0)<<std::endl;
+        cv::Point2d centre(outputt(0,0),outputt(1,0));
+        cv::circle(result,centre,0.5,cv::Scalar(0,255,0));
     }
     std::fclose(stdin);
     cv::imshow("result",result);
